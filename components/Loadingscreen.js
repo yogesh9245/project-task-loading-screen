199eec7bbf618c8656d2482d3d11d06bb46aea82
yogesh9245 <yogeshmanghrani.9245@gmail.com>
2023-11-12 01:19:46 +0530

    Final UI for Loading component. Added 'You think' , 'We create' div and valabs logo. Applied linear gradient to the text

--- components/Loadingscreen.js
+++ components/Loadingscreen.js
@@ -1,71 +1,39 @@
-// // "use client";
-// // // LoadingScreen.js
-// // import React, { useEffect, useState } from 'react';
-
-// // const LoadingScreen = () => {
-// //   const [timer, setTimer] = useState(0);
-
-// //   useEffect(() => {
-// //     // Simulate loading with a timer
-// //     const interval = setInterval(() => {
-// //       // Increase the timer value
-// //       setTimer((prevTimer) => (prevTimer < 100 ? prevTimer + 1 : prevTimer));
-// //     }, 50); // Adjust the interval based on your needs
-
-// //     // Clean up the interval when the component unmounts
-// //     return () => clearInterval(interval);
-// //   }, []);
-
-// //   return (
-// //     <div className="fixed top-0 left-0 w-screen h-screen flex flex-col items-center justify-center bg-blue-400 z-10">
-// //       <div className="relative w-full h-4 bg-white rounded-full overflow-hidden mb-4">
-// //         <div
-// //           className="absolute top-0 left-0 h-full bg-gradient-to-r from-black-500 to-black-100"
-// //           style={{ width: `${timer}%` }}
-// //         ></div>
-// //       </div>
-// //       <div className="text-white text-9xl absolute z-10">{timer}</div>
-// //     </div>
-// //   );
-// // };
-
-// // export default LoadingScreen;
-
-
-// // LoadingScreen.js
-// import React from 'react';
-
-// const LoadingScreen = ({ percentage }) => {
-//   return (
-//     <div className="fixed top-0 left-0 w-screen h-screen flex flex-col items-center justify-center bg-blue-400 z-10">
-//       <div className="relative w-full h-4 bg-white rounded-full overflow-hidden mb-4">
-//         <div
-//           className="absolute top-0 left-0 h-full bg-gradient-to-r from-black-500 to-black-100"
-//           style={{ width: `${percentage}` }}
-//         ></div>
-//       </div>
-//       <div className="text-white text-9xl absolute z-10">{percentage}%</div>
-//     </div>
-//   );
-// };
-
-// export default LoadingScreen;
-
-"use client"
+"use client";
 import React from 'react';
 
 const LoadingScreen = ({ percentage }) => {
+  const gradientStyle = {
+    background: `-webkit-linear-gradient(45deg, white, pink, violet)`,
+    WebkitBackgroundClip: 'text',
+    color: 'transparent',
+  };
+
+  
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen flex flex-col items-center justify-center bg-blue-400 z-10">
-      <div className="relative w-full h-4 bg-white rounded-full overflow-hidden mb-4">
+    <>
+    <div className="fixed top-0 left-0 w-screen h-screen flex flex-col items-center justify-center bg-gray-900 z-10">
+      <div className='text-white text-2xl fixed left-4 top-10 ' style={gradientStyle}>
+      va<span style={{ color: 'white' }}>:LABS</span>
+      </div>
+      <div className="flex justify-between w-full">
+        <div className="text-4xl mb-4 ml-4" style={gradientStyle}>
+          You think
+        </div>
+        <div className="text-4xl mb-4 mr-8" style={gradientStyle}>
+          We create
+        </div>
+      </div>
+      <div className="relative w-full h-4 bg-white rounded-full overflow-hidden mb-4 drop-shadow-2xl">
         <div
-          className="absolute top-0 left-0 h-full bg-gradient-to-r from-black to-white"
+          className="absolute top-0 left-0 h-full bg-gradient-to-r from-white to-pink-500 via-purple-500"
           style={{ width: `${percentage}%` }}
         ></div>
       </div>
       <div className="text-white text-9xl absolute z-10">{percentage}%</div>
     </div>
+    </>
   );
 };
 
 export default LoadingScreen;
+
