e7365461a962441e1d428baf79bf33d2370fc456
yogesh9245 <yogeshmanghrani.9245@gmail.com>
2023-11-11 23:33:43 +0530

    Responsiveness: Both loading screen and 3D elements are made responsive

--- app/page.js
+++ app/page.js
@@ -1,60 +1,8 @@
-// import LoadingScreen from '../components/Loadingscreen';
-// export default function Home() {
-  
-//   return (
-//     <main className="flex min-h-screen flex-col p-24">
-//       <div>Hello</div>
-//       <LoadingScreen/>
-//     </main>
-//   )
-// }
-
-// "use client"
-// import React,{useState} from 'react';
-// import LoadingScreen from '../components/Loadingscreen';
-// import Spline from "@splinetool/react-spline";
-
-// export default function Home() {
-//   const [loading, setLoading] = useState(true)
-//   return (
-//     <main className="flex min-h-screen flex-col p-24">
-//       <div>Hello</div>
-//       {loading && <LoadingScreen /> }
-
-//       <div>
-//         <Spline 
-//         onLoad={()=>setLoading(false)}
-//         scene="https://prod.spline.design/Gy2lrYdDiCKyuqRf/scene.splinecode" />
-//       </div>
-//       <div>
-//         <Spline
-//         onLoad={()=>setLoading(false)}
-//          scene="https://prod.spline.design/Jkhswu59xqkGtptb/scene.splinecode" />
-//       </div>
-//       <div>
-//         <Spline
-//         onLoad={()=>setLoading(false)}
-//          scene="https://prod.spline.design/IVIqXQvsajU0LdRE/scene.splinecode" />
-//       </div>
-//       <div>
-//         <Spline
-//         onLoad={()=>setLoading(false)}
-//          scene="https://prod.spline.design/Gy2lrYdDiCKyuqRf/scene.splinecode" />
-//       </div>
-//       <div>
-//         <Spline
-//         onLoad={()=>setLoading(false)}
-//          scene="https://prod.spline.design/A2uUhI48PQ6qiNP2/scene.splinecode" />
-//       </div>
-//     </main>
-//   )
-// }
-
-
 "use client"
 import React, { useState, useEffect } from 'react';
 import LoadingScreen from '../components/Loadingscreen';
 import Spline from "@splinetool/react-spline";
+
 
 const splineURLs = [
   "https://prod.spline.design/Gy2lrYdDiCKyuqRf/scene.splinecode",
@@ -83,19 +31,69 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
 
   return (
-    <main className="flex min-h-screen flex-col p-24">
+    <main className="flex min-h-screen flex-col p-4 md:p-24">  {/* flex min-h-screen flex-col p-24 */}
       <div>Hello</div>
       {loading && <LoadingScreen percentage={loadingPercentage} />}
 
       {splineURLs.map((url, index) => (
-        <div key={index}>
+        <div key={index} className='mb-4 md:mb-8 lg:mb-12 xl:mb-16'>
           <Spline
             onLoad={handleLoad}
             scene={url}
+            className='w-full h-full md:w-96 lg:w-2/3 xl:w-3/4 mx-auto'
           />
         </div>
       ))}
     </main>
   );
 }
+
+
+// "use client"
+// import React, { useState, useEffect } from 'react';
+// import LoadingScreen from '../components/Loadingscreen';
+// import Spline from "@splinetool/react-spline";
+
+// const splineURLs = [
+//   "https://prod.spline.design/Gy2lrYdDiCKyuqRf/scene.splinecode",
+//   "https://prod.spline.design/Jkhswu59xqkGtptb/scene.splinecode",
+//   "https://prod.spline.design/IVIqXQvsajU0LdRE/scene.splinecode",
+//   "https://prod.spline.design/Gy2lrYdDiCKyuqRf/scene.splinecode",
+//   "https://prod.spline.design/A2uUhI48PQ6qiNP2/scene.splinecode"
+// ];
+
+// export default function Home() {
+//   const [loadedCount, setLoadedCount] = useState(0);
+//   const [loading, setLoading] = useState(true);
+
+//   const handleLoad = () => {
+//     setLoadedCount((prevCount) => prevCount + 1);
+//   };
+
+//   const totalSplineElements = splineURLs.length;
+//   const loadingPercentage = (loadedCount / totalSplineElements) * 100;
+
+//   useEffect(() => {
+//     if (loadedCount === totalSplineElements) {
+//       setLoading(false);
+//     }
+//   }, [loadedCount]);
+
+//   return (
+//     <main className="flex min-h-screen flex-col p-4 md:p-24"> {/* Adjusted padding for larger screens */}
+//       <div className="text-2xl md:text-4xl lg:text-5xl xl:text-6xl mb-4">Hello</div> {/* Responsive text size */}
+//       {loading && <LoadingScreen percentage={loadingPercentage} />}
+
+//       {splineURLs.map((url, index) => (
+//         <div key={index} className="mb-4 md:mb-8 lg:mb-12 xl:mb-16"> {/* Adjusted margin for larger screens */}
+//           <Spline
+//             onLoad={handleLoad}
+//             scene={url}
+//             className="w-full h-full md:w-96 lg:w-2/3 xl:w-3/4 mx-auto"  
+//           />
+//         </div>
+//       ))}
+//     </main>
+//   );
+// }
 
--- components/Loadingscreen.js
+++ components/Loadingscreen.js
@@ -1,38 +1,57 @@
-// "use client";
+// // "use client";
+// // // LoadingScreen.js
+// // import React, { useEffect, useState } from 'react';
+
+// // const LoadingScreen = () => {
+// //   const [timer, setTimer] = useState(0);
+
+// //   useEffect(() => {
+// //     // Simulate loading with a timer
+// //     const interval = setInterval(() => {
+// //       // Increase the timer value
+// //       setTimer((prevTimer) => (prevTimer < 100 ? prevTimer + 1 : prevTimer));
+// //     }, 50); // Adjust the interval based on your needs
+
+// //     // Clean up the interval when the component unmounts
+// //     return () => clearInterval(interval);
+// //   }, []);
+
+// //   return (
+// //     <div className="fixed top-0 left-0 w-screen h-screen flex flex-col items-center justify-center bg-blue-400 z-10">
+// //       <div className="relative w-full h-4 bg-white rounded-full overflow-hidden mb-4">
+// //         <div
+// //           className="absolute top-0 left-0 h-full bg-gradient-to-r from-black-500 to-black-100"
+// //           style={{ width: `${timer}%` }}
+// //         ></div>
+// //       </div>
+// //       <div className="text-white text-9xl absolute z-10">{timer}</div>
+// //     </div>
+// //   );
+// // };
+
+// // export default LoadingScreen;
+
+
 // // LoadingScreen.js
-// import React, { useEffect, useState } from 'react';
+// import React from 'react';
 
-// const LoadingScreen = () => {
-//   const [timer, setTimer] = useState(0);
-
-//   useEffect(() => {
-//     // Simulate loading with a timer
-//     const interval = setInterval(() => {
-//       // Increase the timer value
-//       setTimer((prevTimer) => (prevTimer < 100 ? prevTimer + 1 : prevTimer));
-//     }, 50); // Adjust the interval based on your needs
-
-//     // Clean up the interval when the component unmounts
-//     return () => clearInterval(interval);
-//   }, []);
-
+// const LoadingScreen = ({ percentage }) => {
 //   return (
 //     <div className="fixed top-0 left-0 w-screen h-screen flex flex-col items-center justify-center bg-blue-400 z-10">
 //       <div className="relative w-full h-4 bg-white rounded-full overflow-hidden mb-4">
 //         <div
 //           className="absolute top-0 left-0 h-full bg-gradient-to-r from-black-500 to-black-100"
-//           style={{ width: `${timer}%` }}
+//           style={{ width: `${percentage}` }}
 //         ></div>
 //       </div>
-//       <div className="text-white text-9xl absolute z-10">{timer}</div>
+//       <div className="text-white text-9xl absolute z-10">{percentage}%</div>
 //     </div>
 //   );
 // };
 
 // export default LoadingScreen;
 
-
-// LoadingScreen.js
+"use client"
 import React from 'react';
 
 const LoadingScreen = ({ percentage }) => {
@@ -40,7 +59,7 @@ const LoadingScreen = ({ percentage }) => {
     <div className="fixed top-0 left-0 w-screen h-screen flex flex-col items-center justify-center bg-blue-400 z-10">
       <div className="relative w-full h-4 bg-white rounded-full overflow-hidden mb-4">
         <div
-          className="absolute top-0 left-0 h-full bg-gradient-to-r from-black-500 to-black-100"
+          className="absolute top-0 left-0 h-full bg-gradient-to-r from-black to-white"
           style={{ width: `${percentage}%` }}
         ></div>
       </div>
