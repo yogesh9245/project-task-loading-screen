348eb2414a7c65df2afba6f8a0ab6ca8a894f147
yogesh9245 <yogeshmanghrani.9245@gmail.com>
2023-11-25 17:53:46 +0530

    Added background image to the loading screen. Media queries for the loading percentage. Enabling of You think and We create based on the percentage. Linear gradient for You think and We create

--- components/Loadingscreen.js
+++ components/Loadingscreen.js
@@ -1,56 +1,152 @@
-"use client";
-import React from 'react';
+// "use client";
+// import React from 'react';
 
-const LoadingScreen = ({ percentage }) => {
+// const LoadingScreen = ({ percentage , backgroundImage}) => {
+//   const gradientStyle = {
+//     background: `-webkit-linear-gradient(90deg, #E0E0E0 0%, #FFC3DD 27.48%, #E973C4 58.15%, #9545F6 100%)`,
+//     WebkitBackgroundClip: 'text',
+//     // text-fill-color: transparent;
+//     color: 'transparent',
+//   };
+
+//   return (
+//     <>
+//       <div className="fixed top-0 left-0 w-screen h-screen flex flex-col items-center justify-center z-10 bg-gray-900" style={{ backgroundImage: `url(${backgroundImage})` ,backgroundSize: 'cover'}}>
+       
+
+//         <div className='text-white text-2xl fixed left-4 top-10 ' style={gradientStyle}>
+//           va<span style={{ color: 'white' }}>:LABS</span>
+//         </div>
+//         <div className="flex justify-between w-full mb-8">
+//           <div className="text-4xl ml-4" style={percentage >= 20 ?  gradientStyle: { }}>
+//             You think
+//           </div>
+//           <div className="text-4xl mr-4" style={percentage >=80 ? gradientStyle: { }}>
+//             We create
+//           </div>
+//         </div>
+//         <div className="relative w-full bg-zinc-100 rounded-full mb-4 drop-shadow-2xl" style={{ height: '8px', borderRadius : '4px'}}>
+//           <div
+//             className="absolute top-0 left-0 h-full"
+//             style={{ width: `${ percentage}%`, background:  'linear-gradient(90deg, #E0E0E0 0%, #FFC3DD 27.48%, #E973C4 58.15%, #9545F6 100%)', color: '#DEDEDE' }}
+            
+//           ></div>
+//         </div>
+//         <div style={{fontFamily : 'Syne', fontWeight: 500, fontSize:'220px'}} className="text-white text-9xl absolute z-0 text-zinc-200">{percentage}</div>
+        
+//       </div>
+      
+//       <style>
+//         {`
+//           @media screen and (max-width: 768px) {
+           
+//             .text-2xl {
+//               font-size: 0.5rem; 
+//             }
+//             .text-4xl {
+//               font-size: 1rem; 
+//             }
+//             .text-9xl {
+//               font-size: 2rem;  
+//             }
+//             .mb-8 {
+//               margin-bottom: 1rem; 
+//             }
+//             .ml-4, .mr-4 {
+//               margin-left: 1rem; 
+//               margin-right: 1rem; 
+//             }
+            
+//           }
+//         `}
+//       </style>
+//     </>
+//   );
+// };
+
+// export default LoadingScreen;
+
+
+
+// In LoadingScreen.js
+const LoadingScreen = ({ percentage, backgroundImage }) => {
   const gradientStyle = {
-    background: `-webkit-linear-gradient(45deg, white, pink, violet)`,
+    background: `-webkit-linear-gradient(90deg, #E0E0E0 0%, #FFC3DD 27.48%, #E973C4 58.15%, #9545F6 100%)`,
     WebkitBackgroundClip: 'text',
     color: 'transparent',
   };
 
   return (
     <>
-      <div className="fixed top-0 left-0 w-screen h-screen flex flex-col items-center justify-center bg-gray-900 z-10">
+      <div className="fixed top-0 left-0 w-screen h-screen flex flex-col items-center justify-center z-10 bg-gray-900" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
         <div className='text-white text-2xl fixed left-4 top-10 ' style={gradientStyle}>
           va<span style={{ color: 'white' }}>:LABS</span>
         </div>
         <div className="flex justify-between w-full mb-8">
-          <div className="text-4xl ml-4" style={gradientStyle}>
+          <div className="text-4xl ml-4" style={percentage >= 20 ? gradientStyle : {}}>
             You think
           </div>
-          <div className="text-4xl mr-4" style={gradientStyle}>
+          <div className="text-4xl mr-4" style={percentage >= 80 ? gradientStyle : {}}>
             We create
           </div>
         </div>
-        <div className="relative w-full h-4 bg-white rounded-full overflow-hidden mb-4 drop-shadow-2xl">
+        <div className="relative w-full bg-gray-300 rounded-full mb-4 drop-shadow-2xl" style={{ height: '8px', borderRadius: '4px', position: 'relative' }}>
           <div
-            className="absolute top-0 left-0 h-full bg-gradient-to-r from-white to-pink-500 via-purple-500"
-            style={{ width: `${ percentage}%` }}
+            className="absolute top-0 left-0 h-full -z-50"
+            style={{ width: `${percentage}%`, background: 'linear-gradient(90deg, #E0E0E0 0%, #FFC3DD 27.48%, #E973C4 58.15%, #9545F6 100%)', color: '#DEDEDE' }}
           ></div>
+          <div style={{ fontFamily: 'Syne', fontWeight: 500, position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}} className="text-white font-bold z-40 responsive-font">{percentage}</div>
         </div>
-        <div className="text-white text-9xl absolute z-10">{percentage}%</div>
       </div>
       <style>
         {`
           @media screen and (max-width: 768px) {
-           
             .text-2xl {
-              font-size: 0.5rem; 
+              font-size: 0.5rem;
             }
             .text-4xl {
-              font-size: 1rem; 
+              font-size: 1rem;
             }
             .text-9xl {
-              font-size: 2rem;  
+              font-size: 2rem;
             }
             .mb-8 {
-              margin-bottom: 1rem; 
+              margin-bottom: 1rem;
             }
             .ml-4, .mr-4 {
-              margin-left: 1rem; 
-              margin-right: 1rem; 
+              margin-left: 1rem;
+              margin-right: 1rem;
             }
-            
+          }
+
+          @media screen and (max-width: 768px) {
+            .responsive-font {
+              font-size: 6rem;
+            }
+          }
+
+          @media screen and (min-width: 769px) and (max-width: 1023px) {
+            .responsive-font {
+              font-size: 10rem;
+            }
+          }
+
+          @media screen and (min-width: 1024px) and (max-width: 1279px) {
+            .responsive-font {
+              font-size: 15rem;
+            }
+          }
+
+          @media screen and (min-width: 1280px) and (max-width: 1535px) {
+            .responsive-font {
+              font-size: 15rem;
+            }
+          }
+
+          @media screen and (min-width: 1536px) {
+            .responsive-font {
+              font-size: 15rem;
+            }
           }
         `}
       </style>
@@ -59,3 +155,8 @@ const LoadingScreen = ({ percentage }) => {
 };
 
 export default LoadingScreen;
+
+
+
+
+
